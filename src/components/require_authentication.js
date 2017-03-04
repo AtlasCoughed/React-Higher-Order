922e2271575561d7d079f5bdf1409eb69bc5275e
Michael Tran <michaelhuytran@gmail.com>
2017-03-04 10:33:18 -0800

    [feature] - higher order functions done

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -9,9 +9,22 @@ export default function(ComposedComponent){
 		static contextTypes = {
 			router: React.PropTypes.object
 		};
+
+		componentWillMount(){
+			if(!this.props.authenticated){
+				this.context.router.push('/');
+			}
+		}
+
+		componentWillUpdate(nextProps){
+			if(!nextProps.authenticated){
+				this.context.router.push('/');
+			}
+		}
+
 		render(){
-			console.log(this.context);
 			return <ComposedComponent {...this.props}/>;
+
 		}
 	}
 	function mapStateToProps(state){
